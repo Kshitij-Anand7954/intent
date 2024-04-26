65251c1baddf72d121b2d6869cdffa0eff36cfc3
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-04-26 09:26:32 +0530

    intent

---
+++ app/src/main/java/com/example/intentpassingexample/secondActivity.java
@@ -0,0 +1,38 @@
+package com.example.intentpassingexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        Intent fromact=getIntent();
+        String title=fromact.getStringExtra("title");
+        int roll=fromact.getIntExtra("roll",0);
+        TextView txt=findViewById(R.id.textinfo);
+        txt.setText("roll no.  "+roll+"  Name:   "+title);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Kshitij app");
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
